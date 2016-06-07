e1e69006b69646bafbbbeae5c81c322493a31c3a
Joe Hill <joe@jkh3.com>
2016-06-07 19:48:06 -0400

    Express Server now serving up index.html

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var express = require('express');
 var app = express();
 var multer = require('multer');
 
+app.use(express.static(__dirname)); 
+
+app.get('/', function(req, res) {
+    res.sendFile('/index.html');
+});
+
 
 
 app.listen(process.env.PORT || 3000, function() {
