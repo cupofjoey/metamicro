6705817bb3e468c139ff70f49384feb40969324d
Joe Hill <joe@jkh3.com>
2016-06-07 20:30:47 -0400

    App refreshes but still gives file.send in bytes to the user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 app.use(express.static(__dirname)); 
 
@@ -9,7 +10,22 @@ app.get('/', function(req, res) {
     res.sendFile('/index.html');
 });
 
+app.post('/', upload.single("Upload"), function (req, res) {
+  console.log(req.file); //form files
+	/* example output:
+            { fieldname: 'upl',
+              originalname: 'grumpy.png',
+              encoding: '7bit',
+              mimetype: 'image/png',
+              destination: './uploads/',
+              filename: '436ec561793aa4dc475a88e84776b1b9',
+              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+              size: 277056 }
+	 */
+	res.send(200, "Your File Size: " + req.file.size + " bytes!");
+})
 
+//var storage = multer.fileSize();
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log("Ready!");
